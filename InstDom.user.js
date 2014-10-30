132c5b8f2a819f5f31587ac36d9e9100dc4b3983
Lu Zhang <zhanglu@vt.edu>
2014-10-29 21:43:35 -0400

    jquety

--- InstDom.user.js
+++ InstDom.user.js
@@ -6,9 +6,11 @@
 // == http://www.eventracer.org/race_bug.html
 // @run-at      document-start
 // @require     https://ajax.googleapis.com/ajax/libs/prototype/1.7.2.0/prototype.js
+// @require     http://code.jquery.com/jquery-2.1.0.min.js
 // ==/UserScript==
 
 console.log('  <Inst:>Instrumentation start');
+
 
 
 console.log('  <Inst:>Instrumentation finish');
